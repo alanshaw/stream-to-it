595f98d268906814cafccc7548bfebed0d4d376d
Jacob Heun <jacobheun@gmail.com>
2020-07-13 16:58:29 +0200

    fix: memory leak due to Promise.race (#7)
    Details for why the leak happens are in the issue, https://github.com/alanshaw/stream-to-it/issues/6. This code is heavily influenced by the fix for writeToStream in streaming-iterables but avoids breaking any functionality here.
    
    - Fix leak by removing Promise.race use
    - Adds the [leak reproduction code](https://github.com/alanshaw/stream-to-it/issues/6#issue-589555329) to a benchmarks folder. Run instructions leveraging Clinic.js are inline in the file. I can add to the readme if desired.
    - Updates deps :)
    - Added `package.files` to restrict what gets uploaded (tests were being packaged)
    - Travis now runs Node.js `stable`(14) and `lts`(12)
    
    ## Clinic.js Reports
    
    I've also listed the IPFS CIDs (linked to the gateway) if you want to peruse in detail.
    
    **After** [bafybeidzy5otcicczh43ifdcg55btdl4q5j75vfolhsygr3oomqjavj6p4](https://gateway.ipfs.io/ipfs/bafybeidzy5otcicczh43ifdcg55btdl4q5j75vfolhsygr3oomqjavj6p4)
    ![image](https://user-images.githubusercontent.com/639834/87229565-4ef83480-c3a9-11ea-8b62-222c83931fa6.png)
    
    
    **Before** [bafybeig3sw5t326is6e7rknf7ena3ffubekperpsdgf2dty7w74pkvmhai](https://gateway.ipfs.io/ipfs/bafybeig3sw5t326is6e7rknf7ena3ffubekperpsdgf2dty7w74pkvmhai)
    ![image](https://user-images.githubusercontent.com/639834/87229558-37b94700-c3a9-11ea-8b4a-9d5ca2318f14.png)
    
    ## Package file changes
    ```sh
    $ npm pack --dry-run
    npm notice 
    npm notice 📦  stream-to-it@0.2.0
    npm notice === Tarball Contents === 
    npm notice 1.1kB LICENSE     
    npm notice 151B  duplex.js   
    npm notice 215B  index.js    
    npm notice 2.2kB sink.js     
    npm notice 494B  source.js   
    npm notice 395B  transform.js
    npm notice 1.1kB package.json
    npm notice 3.4kB README.md
    ```
    
    fixes https://github.com/alanshaw/stream-to-it/issues/6

---
+++ benchmark/memory.js
@@ -0,0 +1,27 @@
+// Copied from https://github.com/alanshaw/stream-to-it/issues/6#issue-589555329
+
+/**
+ * Running with Clinic.js
+ *
+ * This will open the Clinic report in your default browser.
+ *
+ * 1. Install Clinic.js `npm install -g clinic`
+ * 2. From project root: `clinic doctor -- node ./benchmark/memory.js`
+ */
+
+const fs = require('fs')
+const os = require('os')
+const toIterable = require('../')
+const pipe = require('it-pipe')
+
+async function * run () {
+  for (let i = 0; i < 170000; i++) {
+    yield Buffer.alloc(4096)
+  }
+}
+
+(async () => {
+  const sink = toIterable.sink(fs.createWriteStream(`${os.tmpdir()}/memcheck.txt`))
+  await pipe(run(), sink)
+  process.exit(1)
+})()
